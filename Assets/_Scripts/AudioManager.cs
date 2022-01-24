ad28c00e5971b4536a0f98ff3a8fd1e5d863f7a7
nuclearMoneky <lyndond@uci.edu>
2022-01-23 18:44:23 -0800

    Cool changes

---
+++ Assets/_Scripts/AudioManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    // public Sound[] sounds;
+    
+    // public static AudioManager instance;
+
+    // private float fadeDuraction = 0.6f;
+
+    // private void Awake() 
+    // {
+    //     if (instance== null) 
+    //     {
+    //         instance = this;
+    //     } else 
+    //     {
+    //         Destroy(gameObject);
+    //         return;
+    //     }
+
+    //     DontDestroyOnLoad(gameObject);
+
+    //     foreach(Sound s in sounds)
+    // }
+}
