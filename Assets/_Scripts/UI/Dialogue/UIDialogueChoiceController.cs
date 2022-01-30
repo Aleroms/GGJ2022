516fce4df1f48f5d82574c1d4cecf9643792c310
nuclearMoneky <lyndond@uci.edu>
2022-01-30 08:27:56 -0800

    Finished Pier Dialogue

--- Assets/_Scripts/UI/Dialogue/UIDialogueChoiceController.cs
+++ Assets/_Scripts/UI/Dialogue/UIDialogueChoiceController.cs
@@ -7,20 +7,22 @@ public class UIDialogueChoiceController : MonoBehaviour
     [SerializeField] private TMP_Text choice;
     [SerializeField] private DialogueChannel dialogueChannel;
 
-    private DialogueNode choiceNextNode;
+    [SerializeField] private DialogueNode choiceNextNode;
 
     public DialogueChoice Choice 
     {
         set 
         {
-            // choice.text = value.ChoicePreview;
-            choice.gameObject.GetComponentInChildren<Text>().text = value.ChoicePreview;
+            choice.text = value.ChoicePreview;
+            // choice.gameObject.GetComponentInChildren<Text>().text = value.ChoicePreview;
             choiceNextNode = value.ChoiceNode;
+            // print(choiceNextNode);
         }
     }
 
     private void Start() 
     {
+        print(GetComponent<Button>());
         GetComponent<Button>().onClick.AddListener(OnClick);
     }
 
